33dbc06454488cdb479b229113c70e78ebaefc68
plitche <kysu728@naver.com>
2023-02-17 19:48:03 +0900

    4. 카운팅 앱 만들기2

--- Project/my_first_flutter_project/lib/main.dart
+++ Project/my_first_flutter_project/lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 // Status less방식과 state ful 방식
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { // 정적
   @override
   Widget build(BuildContext context) { // App을 build
     return MaterialApp(
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget { // 동적
   final String title;
 
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -26,6 +28,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0; // _로 시작하는 변수는 private : class 내부에서만 사용
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,10 +39,22 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('You have pushed the button this many times:'),
-          Text('Counter', style: Theme.of(context).textTheme.displayMedium)
-        ],)
+          Text('$_counter', style: Theme.of(context).textTheme.displayMedium) // 변수는 $_***
+        ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'increment',
+        child: Icon(Icons.add)
       ),
     );
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
   }
 }
 
